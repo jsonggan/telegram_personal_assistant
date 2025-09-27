a5afbccd8f8f37e0f0f068780f9430661f05fe69
jsonggan <94308881+jsonggan@users.noreply.github.com>
2025-09-27 13:40:21 +0800

    init springboot

---
+++ src/main/java/com/assistant/telegrambot/AssistantTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.assistant.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantTelegramBotApplication.class, args);
+	}
+
+}
